4b1cefc0bdf06a308bf3a9f38ab803a91edd5f42
Enmanuel Mendoza <97385184+EnmanuelCoder@users.noreply.github.com>
2023-10-03 21:41:08 -0300

    Update Modal.jsx

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -7,7 +7,7 @@ const Modal = ({product}) => {
   return (
     <article className="w-full h-screen flex justify-center items-center fixed top-0 left-0 bg-colors-black bg-opacity-60 z-50">
       <div className="w-96 h-80 p-5 bg-white rounded-md flex flex-col justify-center border-4 border-primary">
-        <h3 className="text-center text-2xl">Quieres agragrar este producto al carrito de compra?</h3>
+        <h3 className="text-center text-2xl">Quieres agregrar este producto al carrito de compra?</h3>
 
        <div className="mt-5 flex gap-10 justify-center">
           <button onClick={() => {
